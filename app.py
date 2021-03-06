7cd2bb0125e7e24290fbbade325c77ffcb6acf24
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2021-03-06 15:06:36 +0530

    add feature of uploading questions from admin

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask,render_template,redirect,request,session,flash,json
+from flask import Flask,render_template,redirect,request,session,flash,json,url_for
 import db as api
 import credential
+from werkzeug.utils import secure_filename
+import os
+import sys
+import random
+import generator as gen
+
 app = Flask(__name__)
 app.secret_key=credential.secret_key
 
+# for questions 
+# UPLOAD_FOLDER = credential.path
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route('/')
 def home():
-    if 'username' in session:
-        username=session['username']
-        return render_template('index.html')  
-    return redirect('/login')
+    # if 'username' in session:
+    #     username=session['username']
+    #     return render_template('index.html')  
+    # return redirect('/login')
+    return render_template('index.html')
 
 @app.route('/signup',methods=['POST','GET'])
 def sign_up():
@@ -44,5 +56,51 @@ def logout():
     session.pop('username',None)
     flash('You logged out')
     return json.dumps({'status':'logout'})
+
+
+
+@app.route('/api_check_name', methods=['POST'])
+def api_check_name():
+    data=request.get_data().decode("utf-8").split("=")[1]
+    if(api.check_ques_name(data)==True):
+        return json.dumps({'status':'0'})
+    return json.dumps({'status':'1'})
+
+@app.route('/api_question',methods=['POST','GET'])
+def api_question():
+    try:
+        file1 = request.files['file1']
+        file2 = request.files['file2']
+        file3 = request.files['file3']    
+        form_details = request.form
+        l1=""
+        l2=""
+        l3 = ""
+        filename = secure_filename(file1.filename)
+        app.config['UPLOAD_FOLDER']=credential.path
+        file1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        l1=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        filename = secure_filename(file2.filename)
+        app.config['UPLOAD_FOLDER']=credential.path2
+        file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))                
+        l2 = os.path.join(app.config['UPLOAD_FOLDER'], filename)  
+        app.config['UPLOAD_FOLDER']=credential.path3
+        filename = secure_filename(file3.filename)
+        file3.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))        
+        l3 = os.path.join(app.config['UPLOAD_FOLDER'], filename)  
+        q_key=gen.key()
+        while api.check_qkey(q_key)==False:
+            q_key=gen.key()
+        api.new_question(q_key,l1,form_details['question_name'],l2,l3)
+        return json.dumps({'status':'0'})
+    except:
+        return json.dumps({'status':'1'})   
+
+@app.route('/admin', methods=['POST','GET'])
+def new_question():
+    return render_template('admin_question.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/admin_question.js
@@ -0,0 +1,96 @@
+$(document).ready(function () {
+    $("#submit").click(function () {
+        $('#form1').submit(function () {
+            var form = $('#form1')[0];
+            var data = new FormData(form);
+            console.log(typeof(data),"hahahhhahahhah");
+            $.ajax({
+                url: '/api_question',
+                type: 'POST',
+                enctype: 'multipart/form-data',
+                data: data,
+                processData: false,
+                contentType: false,
+                cache: false,
+                success: function (data) {
+                    var obj = JSON.parse(data)
+                    if(obj.status == '0'){
+                        alert('Successfully uploaded');
+                        window.location = 'http://localhost:5000/admin';
+                    }
+                    else{
+                        alert('Failed Question is not uploaded');
+                        window.location = 'http://localhost:5000/admin';
+                    }
+                },
+                error: function (jqXhr, textStatus, errorMessage) { // if any error come then 
+                    $('p').append('Error' + errorMessage);
+                }
+            })
+
+        });
+    });
+    $('#file1').on('change', function () {
+        var fileInput = $('#file1').val();
+        var allowedExtensions = /(\.doc|\.docx|\.odt|\.pdf|\.tex|\.txt|\.rtf|\.wps|\.wks|\.wpd)$/i;
+        if (!allowedExtensions.exec(fileInput)) {
+            $('#file1_ok').text("Wrong Extension");
+            $('#file1_ok').show();
+            $('#file1').val("");
+        }
+        else {
+            $('#file1_ok').hide();
+        }
+    });
+    
+    $('#file2').on('change', function () {
+        var fileInput = $('#file2').val(); 
+        var allowedExtensions = /(\.doc|\.docx|\.odt|\.pdf|\.tex|\.txt|\.rtf|\.wps|\.wks|\.wpd)$/i;
+        if (!allowedExtensions.exec(fileInput)) {
+            $('#file2_ok').text("Wrong Extension");
+            $('#file2_ok').show();
+            $('#file2').val("");
+        }
+        else {
+            $('#file2_ok').hide();
+        }
+    });
+    
+    $('#file3').on('change', function () {
+        var fileInput = $('#file3').val(); 
+        var allowedExtensions = /(\.doc|\.docx|\.odt|\.pdf|\.tex|\.txt|\.rtf|\.wps|\.wks|\.wpd)$/i;
+        if (!allowedExtensions.exec(fileInput)) {
+            $('#file3_ok').text("Wrong Extension");
+            $('#file3_ok').show();
+            $('#file3').val("");
+        }
+        else {
+            $('#file3_ok').hide();
+        }
+    });
+    
+    
+    $("#name").on('input', function () {
+        var name_send = $("#name").val();
+        $.ajax({
+            url: '/api_check_name',
+            type: 'POST',  
+            data: { 'data': name_send },
+            success: function (data) {
+                var obj = JSON.parse(data);
+                if (obj.status == "1") {
+                    $("#name_ok").text("Correct name");
+                    $("#name_ok").show();
+                }
+                else {
+                    $("#name_ok").text("Name Already Exist");
+                    $("#name_ok").show();
+                }
+            },
+            error: function (jqXhr, textStatus, errorMessage) { // if any error come then 
+                $('p').append('Error' + errorMessage);
+            }
+        });
+    });
+});
+
