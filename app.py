289776f77921b9cfe6028cca4ee0ff1956a77899
siddhant <siddhantgupta384@gmail.com>
2021-03-17 16:19:11 +0530

    api crap checked
    .....

--- app.py
+++ app.py
@@ -6,118 +6,65 @@ import os
 import sys
 import random
 import generator as gen
-import mysql.connector
 
 app = Flask(__name__)
 
-conn=mysql.connector.connect(host="localhost",user="root",password="Granger@7",database="brocode", buffered = True)
-# ,auth_plugin="caching_sha2_password"
-cursor=conn.cursor()
 
-# app.secret_key=credential.secret_key
+
 app.secret_key=os.urandom(24)
-
-# if __name__ == '__main__'
-#     app.run(debug=True)
 
 @app.route('/')
 def home():
-    # if 'username' in session:
-    #     username=session['username']
-    #     return render_template('index.html')  
-    # return redirect('/login')
-    # items = api.api_get_all()
-    # print(items)
-    # return render_template('index.html',items=items)
-    # if 'username' in session:
-    #     username=session['username']
-    #     return render_template('index.html')  
-    # # return redirect('/login')
-    if 'user_id' in session:
-        return render_template('index.html') 
-    else: 
-        return redirect('/login') 
+    if 'username' in session:
+        username=session['username']
+        return render_template('index.html',username=username)  
+    return redirect('/login')
+    items = api.api_get_all()
+    print(items)
+    return render_template('index.html',items=items)
 
 @app.route('/signup',methods=['POST','GET'])
 def sign_up():
     if(request.method=='POST'):
         form_details=request.form
-        # print(form_details)
-        # session['username']=request.form['username']
-        # api.signup(form_details['name'],form_details['username'],form_details['password'],form_details['email'],form_details['phone_no'],form_details['gender']) 
+        print(form_details)
+        session['username']=request.form['username']
+        api.signup(form_details['name'],form_details['username'],form_details['password'],form_details['email'],form_details['phone_no'],form_details['gender']) 
         flash('Hey, you signed in')
-        return redirect('/login')
+        return redirect('/')
     return render_template('signup.html')
-
-# @app.route('/login',methods=['POST','GET'])
-# def login():
-#     if(request.method=='POST'):
-#         form_details=request.form
-#         session['username']=request.form['username']
-#         data=api.login(form_details['username'],form_details['password'])
-#         if data == "correctPassword":
-#             flash('Successfully logged in')
-#             return redirect('/')
-#         if data == "wrongPassword":
-#             flash('wrongPassword')
-#             return render_template('login.html')
-#         if data == "UsernameDosenotExit":
-#             flash('UsernameDosenotExit')
-#             return render_template('login.html')      
-#     return render_template('login.html')            
-
-@app.route('/login_validation',methods=['POST'])
-def login_validation():
-    username=request.form.get('username')
-    password=request.form.get('password')
-
-    cursor.execute("""SELECT *FROM `usermaster` WHERE `username` LIKE '{}' AND `password` LIKE '{}' """.format(username,password))
-    users=cursor.fetchall()
-    print(users)
-
-    if len(users)>0:
-        session['user_id']=users[0][0]
-        return render_template("index.html",username=username)
-    else:
-        return redirect('/login')
-
-    # return "the emial is {} and the password is {} ".format(username,password)
-@app.route('/add_user',methods=['POST'])
-def add_user():
-    name=request.form.get('name')
-    email=request.form.get('email')
-    password=request.form.get('password')
-    username=request.form.get('username')
-    phone_no=request.form.get('phone_no')
-    gender=request.form.get('gender')
-    cursor.execute("""INSERT INTO `usermaster` (`sno`,`name`,`username`,`password`,`email`,`phone_no`,`gender`) VALUES (NULL,'{}','{}','{}','{}','{}','{}') """.format(name,username,password,email,phone_no,gender))
-    conn.commit()
-
-    cursor.execute(""" SELECT *FROM  `usermaster` WHERE `username` LIKE '{}' """.format(username))
-    myuser=cursor.fetchall()
-    session['user_id']=myuser[0][0]
-    return render_template("index.html",username=username)
 
 @app.route('/login',methods=['POST','GET'])
 def login():
-    if 'user_id' in session:
-        return render_template('index.html')
-    # if request.method =='POST':
-    #     session.pop('user',None)
-    #     # data=api.login(form_details['username'],form_details['password'])
-    #     if request.form['password'] == 'password':
-    #         session['user'] = request.form['username']
-    #         flash('Successfully logged innnn')
-    #         return redirect(url_for('logged_in'))
-    return render_template('login.html')
+    if(request.method=='POST'):
+        form_details=request.form
+        session['username']=request.form['username']
+        print(request.form['username'])
+        data=api.login(form_details['username'],form_details['password'])
+        if data == "correctPassword":
+            # flash('Successfully logged in')
+            print('Successfully logged in')
+            return render_template('index.html',username=request.form['username'])
+        if data == "wrongPassword":
+            # flash('wrongPassword')
+            print('wrongPassword')
+            return render_template('login.html')
+        if data == "UsernameDosenotExit":
+            # flash('UsernameDosenotExit')
+            print('UsernameDosenotExit')
+            return render_template('login.html')      
+    return render_template('login.html')            
 
 
 @app.route('/logout')
 def logout():
-    session.pop('user_id')
+    # session.pop('user_id')
     return redirect('/login')
     # flash('You logged out')
     # return json.dumps({'status':'logout'})
+
+
+
 
 
 
@@ -165,11 +112,7 @@ def new_question():
 
 
 
-@app.route('/logged_in')
-def logged_in():
-    # if g.user:
-    #     return render_template('logged_in.html',user=session['user'])
-    return render_template('logged_in.html')
+
 
 @app.before_request
 def before_request():
--- db.py
+++ db.py
@@ -1,135 +1,135 @@
-# import pymysql
-# import credential
+import pymysql
+import credential
 
 
-# def signup(name,username,password,email,phone_no,gender):
-#     conn=pymysql.connect(
-#         host=credential.host,
-#         port=credential.port,
-#         user=credential.user,
-#         password=credential.password,
-#         db=credential.databasename
-#     )
-#     try:
-#         with conn.cursor() as curr:
-#             sql = "insert into usermaster (name,username,password,email,phone_no,gender) value (%s,%s,%s,%s,%s,%s)"
-#             phone_no=int(phone_no)
-#             curr.execute(sql,(name,username,password,email,phone_no,gender))
-#             conn.commit()
-#     except Exception as e:
-#         print(e)        
+def signup(name,username,password,email,phone_no,gender):
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )
+    try:
+        with conn.cursor() as curr:
+            sql = "insert into usermaster (name,username,password,email,phone_no,gender) value (%s,%s,%s,%s,%s,%s)"
+            phone_no=int(phone_no)
+            curr.execute(sql,(name,username,password,email,phone_no,gender))
+            conn.commit()
+    except Exception as e:
+        print(e)        
 
 
-# def login(username,password):
-#     conn=pymysql.connect(
-#         host=credential.host,
-#         port=credential.port,
-#         user=credential.user,
-#         password=credential.password,
-#         db=credential.databasename
-#     )
-#     try:
-#         with conn.cursor() as curr:
-#             sql = "select password from usermaster where username=(%s)"
-#             curr.execute(sql,username)
-#             output=curr.fetchone()
-#             print(output)
-#             if(output):
-#                 if(password == output[0]):
-#                     return "correctPassword"
-#                 else: return "wrongPassword"    
-#             else:
-#                 return "UsernameDosenotExit"    
-#     except Exception as e:
-#         print(e)       
+def login(username,password):
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )
+    try:
+        with conn.cursor() as curr:
+            sql = "select password from usermaster where username=(%s)"
+            curr.execute(sql,username)
+            output=curr.fetchone()
+            print(output)
+            if(output):
+                if(password == output[0]):
+                    return "correctPassword"
+                else: return "wrongPassword"    
+            else:
+                return "UsernameDosenotExit"    
+    except Exception as e:
+        print(e)       
 
-# def new_question(ques_id,ques_location,ques_name,ques_test_case_file,ques_sol):
-#     conn=pymysql.connect(
-#         host=credential.host,
-#         port=credential.port,
-#         user=credential.user,
-#         password=credential.password,
-#         db=credential.databasename
-#     )
-#     try:
-#         with conn.cursor() as curr:
-#             sql = "insert into ques_master (ques_id,ques_location,ques_name,ques_test_case_file,ques_sol) values (%s,%s,%s,%s,%s)"
-#             # print(ques_id,ques_location,ques_name,ques_test_case_file)
-#             curr.execute(sql,(ques_id,ques_location,ques_name,ques_test_case_file,ques_sol))
-#             # print(ques_id,ques_location,ques_name,ques_test_case_file)
-#             conn.commit()
-#     except Exception as e:
-#         print(e)            
+def new_question(ques_id,ques_location,ques_name,ques_test_case_file,ques_sol):
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )
+    try:
+        with conn.cursor() as curr:
+            sql = "insert into ques_master (ques_id,ques_location,ques_name,ques_test_case_file,ques_sol) values (%s,%s,%s,%s,%s)"
+            # print(ques_id,ques_location,ques_name,ques_test_case_file)
+            curr.execute(sql,(ques_id,ques_location,ques_name,ques_test_case_file,ques_sol))
+            # print(ques_id,ques_location,ques_name,ques_test_case_file)
+            conn.commit()
+    except Exception as e:
+        print(e)            
 
-# def check_qkey(key):
-#     conn=pymysql.connect(
-#         host=credential.host,
-#         port=credential.port,
-#         user=credential.user,
-#         password=credential.password,
-#         db=credential.databasename
-#     )
-#     try:
-#         with conn.cursor() as curr:
-#             sql = "select exist(select* from ques_master where ques_id=(%s))"
-#             curr.execute(sql,(key))
-#             output = curr.fetchone()
-#             if(output==1):
-#                 return False
-#             return True 
-#     except Exception as e:
-#         print(e)        
+def check_qkey(key):
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )
+    try:
+        with conn.cursor() as curr:
+            sql = "select exist(select* from ques_master where ques_id=(%s))"
+            curr.execute(sql,(key))
+            output = curr.fetchone()
+            if(output==1):
+                return False
+            return True 
+    except Exception as e:
+        print(e)        
 
-# def check_ques_name(data):
-#     conn=pymysql.connect(
-#         host=credential.host,
-#         port=credential.port,
-#         user=credential.user,
-#         password=credential.password,
-#         db=credential.databasename
-#     )
-#     try:
-#         with conn.cursor() as curr:
-#             sql = "select * from ques_master where ques_name=(%s) limit 1"
-#             curr.execute(sql,(data))
-#             output = curr.fetchone()
-#             if(str(output)=="None"):
-#                 return False
-#             return True
-#     except Exception as e:
-#         print(e)         
+def check_ques_name(data):
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )
+    try:
+        with conn.cursor() as curr:
+            sql = "select * from ques_master where ques_name=(%s) limit 1"
+            curr.execute(sql,(data))
+            output = curr.fetchone()
+            if(str(output)=="None"):
+                return False
+            return True
+    except Exception as e:
+        print(e)         
 
-# def api_get_all():
-#     conn=pymysql.connect(
-#         host=credential.host,
-#         port=credential.port,
-#         user=credential.user,
-#         password=credential.password,
-#         db=credential.databasename
-#     )   
-#     try:
-#         with conn.cursor() as curr:
-#             sql = "select ques_location,ques_name from ques_master"
-#             curr.execute(sql)
-#             output = curr.fetchall()
-#             output = dict(output)
-#             return output
-#     except Exception as e:
-#         print(e)      
+def api_get_all():
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )   
+    try:
+        with conn.cursor() as curr:
+            sql = "select ques_location,ques_name from ques_master"
+            curr.execute(sql)
+            output = curr.fetchall()
+            output = dict(output)
+            return output
+    except Exception as e:
+        print(e)      
 
-# def api_get_id():
-#     conn=pymysql.connect(
-#         host=credential.host,
-#         port=credential.port,
-#         user=credential.user,
-#         password=credential.password,
-#         db=credential.databasename
-#     )   
-#     try:
-#         with conn.cursor() as curr:
-#             sql = "select ques_id from ques_master"
-#             curr.execute(sql)
-#             output = curr.fetchall()
-#             return output
-#     except Exception as e:
-#         print(e)  
+def api_get_id():
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )   
+    try:
+        with conn.cursor() as curr:
+            sql = "select ques_id from ques_master"
+            curr.execute(sql)
+            output = curr.fetchall()
+            return output
+    except Exception as e:
+        print(e)  
