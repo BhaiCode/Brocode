32be3ba5af7709d97a2c086a98d60b0cfe0f6db3
Siddhant Gupta <siddhantgupta384@gmail.com>
2021-04-16 18:03:11 +0530

    loader added
    phew

---
+++ static/js/scriptsss.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    renderer: 'svg',
+    loop:true,
+    autoplay:true,
+    path:'/static/json/45-clock-time-outline-edited.json'
+})
